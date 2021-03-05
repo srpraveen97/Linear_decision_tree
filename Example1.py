0f7510a793a343215c5d12a502d1b56103d74630
Praveen Sundaresan Ramesh <srpraveen1997@gmail.com>
2021-03-05 17:59:49 -0500

    Add files via upload

--- Example1.py
+++ Example1.py
@@ -7,22 +7,22 @@ col1 = np.linspace(1,150,150)
 col2 = []
 for x in col1:
     if x <= 50:
-        col2.append(2*x + 3 + np.random.normal(loc=0,scale=0.5))
+        col2.append(0.3*x + 6 + np.random.normal(loc=0,scale=0.5))
     elif x > 50 and x <= 100:
-        col2.append( 7 + np.random.normal(loc=0,scale=0.5))
+        col2.append(7 + np.random.normal(loc=0,scale=0.5))
     else:
-        col2.append(-3*x + 5 + np.random.normal(loc=0,scale=0.5))
+        col2.append(-0.1*x + 5 + np.random.normal(loc=0,scale=0.5))
 mydata = pd.DataFrame({'col1':col1,'col2':col2})
 
 co1 = [20,43,67,89,121,139]
 co2 = []
 for x in co1:
     if x <= 50:
-        co2.append(2*x + 3)
+        co2.append(0.3*x + 6)
     elif x > 50 and x <= 100:
-        co2.append( 7)
+        co2.append(7)
     else:
-        co2.append(-3*x + 5) 
+        co2.append(-0.1*x + 5) 
 mydata1 = pd.DataFrame({'co1':co1,'co2':co2})
 
 X_train = mydata.to_numpy()[:,[0]]
@@ -30,6 +30,7 @@ y_train = mydata.to_numpy()[:,1]
 
 X_test = mydata1.to_numpy()[:,[0]]
 y_test = mydata1.to_numpy()[:,1]
+
 
 tree = lmt(reg_features=[0])
 model = tree.fit(X_train,y_train)
